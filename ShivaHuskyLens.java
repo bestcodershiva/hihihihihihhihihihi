29f5c54586b93a5982a24940151f65d6f3d0245f
bestcodershiva <158235345+bestcodershiva@users.noreply.github.com>
2024-04-09 18:53:45 -0700

    Add files via upload

---
+++ ShivaHuskyLens.java
@@ -0,0 +1,51 @@
+// package org.firstinspires.ftc.teamcode;
+// 
+// import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+// import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+// 
+// @TeleOp(name="ShivaHuskyLens")
+// 
+// public class ShivaHuskyLens extends LinearOpMode{
+// 
+//     // todo: write your code here
+//     WarriorsMyRobotConfig2 robotConfig=new WarriorsMyRobotConfig2();
+//     String genStratus="Hello Drivers";
+//     String armStratus="Ready";
+//     String driveSpeed="Normal";
+//     String turnSpeed="Normal";
+// 
+// 
+//     public void updateDriverWithSystemStratus(){
+//         telemetry.clear();
+//         telemetry.addLine();
+//         telemetry.addData("Stratus",genStratus);
+//         telemetry.addLine();
+//         telemetry.addData("Arm", armStratus);
+//         
+//         telemetry.addLine();
+//         telemetry.addData("Drive Speed", driveSpeed);
+//         
+//         telemetry.addLine();
+//         telemetry.addData("Turn Speed", turnSpeed);
+//         
+//         telemetry.update();
+//     
+//     }
+//   
+// }
+// 
+//     public boolean isRedVisibleToCamera() 
+//         boolean RedAhead = false;
+//         for (int j = 0; j < 5; j++) {
+//             HuskyLens.Block[] blocks = robotconfig.huskyLens.block();
+//             telemetry.addData("Block count", block.length);
+//             telemetry.update();
+//             for (int i = 0; i < blocks.length; i++) {
+//                 telemetry.addData
+//             }
+//             
+//         }
+//             }
+// 
+// 
+// 
